6fb42ccbd875f94e596569db4bc098c29365a29f
olivermunkuli <olivermunkuli@outlook.com>
2023-01-20 16:40:53 +0200

    checking in before using axios

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,12 +1,12 @@
 import { useState, useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext';
 import AlertContext from '../../context/alert/AlertContext';
-// import { searchUsers } from '../../context/github/GithubActions'
+import { searchUsers } from '../../context/github/GithubActions';
 
 function UserSearch() {
   const [text, setText] = useState('');
 
-  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
@@ -17,12 +17,9 @@ function UserSearch() {
     if (text === '') {
       setAlert('Please enter something', 'error');
     } else {
-      searchUsers(text);
-
-      // dispatch({ type: 'SET_LOADING' });
-      // const users = await searchUsers(text);
-      // dispatch({ type: 'GET_USERS', payload: users });
-
+      dispatch({ type: 'SET_LOADING' });
+      const users = await searchUsers(text);
+      dispatch({ type: 'GET_USERS', payload: users });
       setText('');
     }
   };
@@ -54,8 +51,7 @@ function UserSearch() {
       {users.length > 0 && (
         <div>
           <button
-            onClick={clearUsers}
-            // onClick={() => dispatch({ type: 'CLEAR_USERS' })}
+            onClick={() => dispatch({ type: 'CLEAR_USERS' })}
             className='btn btn-ghost btn-lg'
           >
             Clear
